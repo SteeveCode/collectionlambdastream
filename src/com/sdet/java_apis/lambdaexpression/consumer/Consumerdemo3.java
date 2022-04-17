d2f2c6bffff5fe42980b1f047ea95694d60a89d0
SteeveCode <54332602+SteeveCode@users.noreply.github.com>
2022-04-17 07:42:50 +0100

    Update Consumerdemo3.java

--- src/com/sdet/java_apis/lambdaexpression/consumer/Consumerdemo3.java
+++ src/com/sdet/java_apis/lambdaexpression/consumer/Consumerdemo3.java
@@ -8,5 +8,9 @@ public class Consumerdemo3 {
         Consumer<String> cn2 = s-> System.out.println(s + " has four legs");
         Consumer<String> cn3 = s-> System.out.println(s + " eats grass");
         cn1.andThen(cn2).andThen(cn3).accept("cow");
+        
+        // another way to write the code above is to store the chained consumer interfaces in a consumer variable and then call the accept() on the variable
+        Consumer<String> c4 = cn1.andThen(cn2).andThen(cn3);
+        c4.accept();
     }
 }
